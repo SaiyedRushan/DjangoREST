28897ccf1548f937e02b6502199d9206f62f74c2
SaiyedRushan <rushan52@gmail.com>
2024-02-25 10:53:05 -0500

    chagne dockerfile and dockercompose to run server using gunicorn

--- backend/drf/settings.py
+++ backend/drf/settings.py
@@ -7,10 +7,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
